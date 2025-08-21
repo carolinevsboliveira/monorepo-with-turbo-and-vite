0deab3920cd9826ed8d9e5bfd77111dff5296db1
carolinevsboliveira <carolinevsboliveira@gmail.com>
2025-08-20 23:30:22 -0300

    chore: setup files

---
+++ apps/main/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 text-green-500 p-4 rounded-lg shadow-lg">
+        Hello World - Tailwind Test
+      </div>
+      <div className="mt-4 p-6 bg-blue-500 text-white rounded-xl">
+        Another Tailwind Test Element
+      </div>
+    </>
+  )
+}
+
+export default App
